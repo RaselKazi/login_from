462a6ddbfee4daa4b7279f0ce073bef79e7756db
Rasel Kazi <67517631+RaselKazi@users.noreply.github.com>
2022-08-23 19:27:40 +0600

    fix

--- code.js
+++ code.js
@@ -9,3 +9,16 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
+
+const togglePassword = document.querySelector("#togglePassword");
+        const password = document.querySelector(".password");
+
+        togglePassword.addEventListener("click", function () {
+            // toggle the type attribute
+            const type = password.getAttribute("type") === "password" ? "text" : "password";
+            password.setAttribute("type", type);
+            
+            // toggle the icon
+            this.classList.toggle("bi-eye");
+        });
